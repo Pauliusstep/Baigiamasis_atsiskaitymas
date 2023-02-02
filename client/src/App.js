bc236b2e8745eea2233793103a0b938eec7b06cb
Pauliusstep <you@example.com>
2023-02-02 18:50:25 +0200

    Projekto pradzia

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import './App.css';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: red;
+`;
+
+function App() {
+  return (
+    <div>
+      <Title>Baigiamasis</Title>
+    </div>
+  );
+}
+
+export default App;
