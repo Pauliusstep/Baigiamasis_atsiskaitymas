1c57a8ab356167e5edc1e17eb8bead6ed56ff19b
Pauliusstep <you@example.com>
2023-02-04 21:05:11 +0200

    Styling

--- client/src/pages/Attendees/Attendees.jsx
+++ client/src/pages/Attendees/Attendees.jsx
@@ -1,11 +1,29 @@
 import { useEffect, useState } from "react"
+import { API_URL, LOGGED_IN_USER } from "../../constants/constants";
+import styled from 'styled-components';
+
+const AttendeesList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    list-style: none;
+    width: 500px;
+`;
+
+const AttendeesListItem = styled.li`
+    border-radius: 10px;
+    box-shadow: 0 5px 7px -1px rgb(51 51 51 /23%);
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 30px 10px 30px;
+`;
 
 export const Attendees = () => {
     const [attendees, setAttendees] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8080/attendees?userId=1')
+        fetch(`${API_URL}/attendees?userId=${LOGGED_IN_USER.id}`)
             .then(res => res.json())
             .then(data => {
                 setAttendees(data);
@@ -18,8 +36,15 @@ export const Attendees = () => {
     }
 
     return (
-        <div>
-            {attendees.map((att) => <div key={att.id}>{att.name}</div>)}
-        </div>
+        <AttendeesList>
+            {attendees.map((att) => (
+                <AttendeesListItem key={att.id}>
+                    <span>Vardas: {att.name}</span>
+                    <span>Pavardė: {att.surname}</span>
+                    <span>El.paštas: {att.email}</span>
+                    <span>Tel.Nr: {att.phone}</span>
+                </AttendeesListItem>
+            ))}
+        </AttendeesList>
     );
 }
